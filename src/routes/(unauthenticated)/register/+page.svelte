a25c04ddc0e4fdd913ec6b6fe0cf101c9423e068
ISALAE <isalae@edu.umea.se>
2023-06-08 00:01:02 +0200

    Yes

--- src/routes/(unauthenticated)/register/+page.svelte
+++ src/routes/(unauthenticated)/register/+page.svelte
@@ -4,9 +4,16 @@
 	export let form: ActionData;
 </script>
 
+<style>
+	.placeholder {
+		border-radius: 5px;
+		padding-left: 5px;
+	}
+</style>
+
 <form method="POST" action="?/register">
-	<input style="margin-left: 5px;" type="username" name="username" />
-	<input type="password" name="password" />
+	<input style="margin-left: 5px;" type="username" name="username" placeholder="username" class="placeholder" />
+	<input type="password" name="password" placeholder="password" class="placeholder"/>
 	<button>REGISTER</button>
 	{#if form?.message}
 		<p>{form?.message}</p> 
